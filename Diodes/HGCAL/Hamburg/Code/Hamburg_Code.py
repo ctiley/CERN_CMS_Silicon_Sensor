612dea7cf845341d3aa57062f653c45190924250
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2022-02-03 19:28:08 -0500

    Update

--- Diodes/HGCAL/Hamburg/Code/Hamburg_Code.py
+++ Diodes/HGCAL/Hamburg/Code/Hamburg_Code.py
@@ -4,7 +4,6 @@ from scipy.optimize import curve_fit
 import pandas as pd
 from matplotlib.pyplot import figure
 
-#%%
 
 neff_N4789_20_LL_DIODE = []
 neff_N4789_20_LL_DIODEQUARTER = []
@@ -26,6 +25,11 @@ neff_N4789_20_LL_DIODEQUARTER = df_N4789_20_LL_DIODEQUARTER['Neff']
 neff_N6789_24_LL_DIODE = df_N6789_24_LL_DIODE['Neff']
 neff_N6789_24_LL_DIODEQUARTER = df_N6789_24_LL_DIODEQUARTER['Neff']
 
+len_N4789_20_LL_DIODE = len(df_N4789_20_LL_DIODE) - 1
+len_N4789_20_LL_DIODEQUARTER = len(df_N4789_20_LL_DIODEQUARTER) - 1
+len_N6789_24_LL_DIODE = len(df_N6789_24_LL_DIODE)- 1 
+len_N6789_24_LL_DIODEQUARTER = len(df_N6789_24_LL_DIODEQUARTER) - 1
+
 #%%
 
 fluence = 6.5*10**18
@@ -35,47 +39,57 @@ guess = np.array([.9646, -3.5*10**18, 1.4, 63, 90])
 def Hamburg(x, ga, NC, gy, ta, ty):
     return ga*np.exp(-x/ta)*fluence + gy*(1.-1./(1.+x/ty))*fluence + NC
 
-annealing_N4789_20_LL_DIODE_fit = np.linspace(annealing_N4789_20_LL_DIODE[0], annealing_N4789_20_LL_DIODE[18], 1000)
-annealing_N4789_20_LL_DIODEQUARTER_fit = np.linspace(annealing_N4789_20_LL_DIODEQUARTER[0], annealing_N4789_20_LL_DIODEQUARTER[4], 1000)
-annealing_N6789_24_LL_DIODE_fit = np.linspace(annealing_N6789_24_LL_DIODE[0], annealing_N6789_24_LL_DIODE[18], 1000)
-annealing_N6789_24_LL_DIODEQUARTER_fit = np.linspace(annealing_N6789_24_LL_DIODEQUARTER[0], annealing_N6789_24_LL_DIODEQUARTER[4], 1000)
+annealing_N4789_20_LL_DIODE_fit = np.linspace(annealing_N4789_20_LL_DIODE[0], annealing_N4789_20_LL_DIODE[len_N4789_20_LL_DIODE], 1000)
+annealing_N4789_20_LL_DIODEQUARTER_fit = np.linspace(annealing_N4789_20_LL_DIODEQUARTER[0], annealing_N4789_20_LL_DIODEQUARTER[len_N4789_20_LL_DIODEQUARTER], 1000)
+annealing_N6789_24_LL_DIODE_fit = np.linspace(annealing_N6789_24_LL_DIODE[0], annealing_N6789_24_LL_DIODE[len_N6789_24_LL_DIODE], 1000)
+annealing_N6789_24_LL_DIODEQUARTER_fit = np.linspace(annealing_N6789_24_LL_DIODEQUARTER[0], annealing_N6789_24_LL_DIODEQUARTER[len_N6789_24_LL_DIODEQUARTER], 1000)
 
 Params_N4789_20_LL_DIODE, Covariances_N4789_20_LL_DIODE = curve_fit(Hamburg, annealing_N4789_20_LL_DIODE, neff_N4789_20_LL_DIODE, guess)
 Params_N4789_20_LL_DIODEQUARTER, Covariances_N4789_20_LL_DIODEQUARTER = curve_fit(Hamburg, annealing_N4789_20_LL_DIODEQUARTER, neff_N4789_20_LL_DIODEQUARTER, guess)
 Params_N6789_24_LL_DIODE, Covariances_N6789_24_LL_DIODE = curve_fit(Hamburg, annealing_N6789_24_LL_DIODE, neff_N6789_24_LL_DIODE, guess)
 Params_N6789_24_LL_DIODEQUARTER, Covariances_N6789_24_LL_DIODEQUARTER = curve_fit(Hamburg, annealing_N6789_24_LL_DIODEQUARTER, neff_N6789_24_LL_DIODEQUARTER, guess)
 
-neff_N4789_20_LL_DIODE_fit = Params_N4789_20_LL_DIODE[0]*np.exp(-annealing_N4789_20_LL_DIODE_fit/Params_N4789_20_LL_DIODE[3])*fluence + Params_N4789_20_LL_DIODE[2]*(1.-1./(1.+annealing_N4789_20_LL_DIODE_fit/Params_N4789_20_LL_DIODE[4]))*fluence + Params_N4789_20_LL_DIODE[1]
-neff_N4789_20_LL_DIODEQUARTER_fit = Params_N4789_20_LL_DIODEQUARTER[0]*np.exp(-annealing_N4789_20_LL_DIODEQUARTER_fit/Params_N4789_20_LL_DIODEQUARTER[3])*fluence + Params_N4789_20_LL_DIODEQUARTER[2]*(1.-1./(1.+annealing_N4789_20_LL_DIODEQUARTER_fit/Params_N4789_20_LL_DIODEQUARTER[4]))*fluence + Params_N4789_20_LL_DIODEQUARTER[1]
-neff_N6789_24_LL_DIODE_fit = Params_N6789_24_LL_DIODE[0]*np.exp(-annealing_N6789_24_LL_DIODE_fit/Params_N6789_24_LL_DIODE[3])*fluence + Params_N6789_24_LL_DIODE[2]*(1.-1./(1.+annealing_N6789_24_LL_DIODE_fit/Params_N6789_24_LL_DIODE[4]))*fluence + Params_N6789_24_LL_DIODE[1]
-neff_N6789_24_LL_DIODEQUARTER_fit = Params_N6789_24_LL_DIODEQUARTER[0]*np.exp(-annealing_N6789_24_LL_DIODEQUARTER_fit/Params_N6789_24_LL_DIODEQUARTER[3])*fluence + Params_N6789_24_LL_DIODEQUARTER[2]*(1.-1./(1.+annealing_N6789_24_LL_DIODEQUARTER_fit/Params_N6789_24_LL_DIODEQUARTER[4]))*fluence + Params_N6789_24_LL_DIODEQUARTER[1]
+neff_N4789_20_LL_DIODE_fit = (Params_N4789_20_LL_DIODE[0]*np.exp(-annealing_N4789_20_LL_DIODE_fit/Params_N4789_20_LL_DIODE[3])*fluence + 
+                                     Params_N4789_20_LL_DIODE[2]*(1.-1./(1.+annealing_N4789_20_LL_DIODE_fit/Params_N4789_20_LL_DIODE[4]))*fluence + Params_N4789_20_LL_DIODE[1])
+
+neff_N4789_20_LL_DIODEQUARTER_fit = (Params_N4789_20_LL_DIODEQUARTER[0]*np.exp(-annealing_N4789_20_LL_DIODEQUARTER_fit/Params_N4789_20_LL_DIODEQUARTER[3])*fluence + 
+                                     Params_N4789_20_LL_DIODEQUARTER[2]*(1.-1./(1.+annealing_N4789_20_LL_DIODEQUARTER_fit/Params_N4789_20_LL_DIODEQUARTER[4]))*fluence + 
+                                     Params_N4789_20_LL_DIODEQUARTER[1])
+
+neff_N6789_24_LL_DIODE_fit = (Params_N6789_24_LL_DIODE[0]*np.exp(-annealing_N6789_24_LL_DIODE_fit/Params_N6789_24_LL_DIODE[3])*fluence + 
+                              Params_N6789_24_LL_DIODE[2]*(1.-1./(1.+annealing_N6789_24_LL_DIODE_fit/Params_N6789_24_LL_DIODE[4]))*fluence + Params_N6789_24_LL_DIODE[1])
+
+neff_N6789_24_LL_DIODEQUARTER_fit = (Params_N6789_24_LL_DIODEQUARTER[0]*np.exp(-annealing_N6789_24_LL_DIODEQUARTER_fit/Params_N6789_24_LL_DIODEQUARTER[3])*fluence + 
+                                     Params_N6789_24_LL_DIODEQUARTER[2]*(1.-1./(1.+annealing_N6789_24_LL_DIODEQUARTER_fit/Params_N6789_24_LL_DIODEQUARTER[4]))*fluence + 
+                                     Params_N6789_24_LL_DIODEQUARTER[1])
 
 #%%
 
-figure(figsize=(10, 10), dpi=100)
+figure(figsize=(10, 6), dpi=100)
 
 plt.suptitle('HGCal Campaign Round 2.3', fontsize=20)
-plt.scatter(71.92619261926193,9.11857E+18, label = 'N4789_20_LL_DIODE Minimum at 71.93 min')
-plt.scatter(71.92619261926193,9.11857E+18, label = 'N4789_20_LL_DIODEQUARTER Minimum at 71.93 min')
 plt.scatter(annealing_N4789_20_LL_DIODE, neff_N4789_20_LL_DIODE, label = r'N4789_20_LL_DIODE $N_{eff}$ Data')
 plt.scatter(annealing_N4789_20_LL_DIODEQUARTER, neff_N4789_20_LL_DIODEQUARTER, label = r'N4789_20_LL_DIODEQUARTER $N_{eff}$ Data')
 plt.plot(annealing_N4789_20_LL_DIODE_fit, neff_N4789_20_LL_DIODE_fit, label = r'N4789_20_LL_DIODE $N_{eff}$ Fit')
 plt.plot(annealing_N4789_20_LL_DIODEQUARTER_fit, neff_N4789_20_LL_DIODEQUARTER_fit, label = r'N4789_20_LL_DIODEQUARTER $N_{eff}$ Fit')
+plt.scatter(40.0,1.804447754051017e+19, label = 'N4789_20_LL_DIODE Minimum at 40.0 min')
+plt.scatter(37.65765765765766,2.0283738609012146e+19, label = 'N4789_20_LL_DIODEQUARTER Minimum at 37.7 min')
 plt.xlabel('Annealing (min)', fontsize=22)
 plt.ylabel(r'$N_{eff}$  $\propto$ $V_{depletion}$', fontsize=22)
-plt.legend(fontsize = 18)
+plt.legend(fontsize = 14)
 
 #%%
 
 figure(figsize=(10, 10), dpi=100)
 
 plt.suptitle('HGCal Campaign Round 2.2', fontsize=20)
-plt.scatter(71.92619261926193,9.11857E+18, label = 'N6789_24_LL_DIODE Minimum at 71.93 min')
-plt.scatter(71.92619261926193,9.11857E+18, label = 'N6789_24_LL_DIODEQUARTER Minimum at 71.93 min')
 plt.scatter(annealing_N6789_24_LL_DIODE, neff_N6789_24_LL_DIODE, label = r'N6789_24_LL_DIODE $N_{eff}$ Data')
 plt.scatter(annealing_N6789_24_LL_DIODEQUARTER, neff_N6789_24_LL_DIODEQUARTER, label = r'N6789_24_LL_DIODEQUARTER $N_{eff}$ Data')
 plt.plot(annealing_N6789_24_LL_DIODE_fit, neff_N6789_24_LL_DIODE_fit, label = r'N6789_24_LL_DIODE $N_{eff}$ Fit')
 plt.plot(annealing_N6789_24_LL_DIODEQUARTER_fit, neff_N6789_24_LL_DIODEQUARTER_fit, label = r'N6789_24_LL_DIODEQUARTER $N_{eff}$ Fit')
+plt.scatter(2.3223223223223224,2.193492074836568e+19, label = 'N6789_24_LL_DIODE Minimum at 71.93 min')
+plt.scatter(16.896896896896898,2.3627130148287525e+19, label = 'N6789_24_LL_DIODEQUARTER Minimum at 71.93 min')
+
 plt.xlabel('Annealing (min)', fontsize=22)
 plt.ylabel(r'$N_{eff}$  $\propto$ $V_{depletion}$', fontsize=22)
 plt.legend(fontsize = 18)
