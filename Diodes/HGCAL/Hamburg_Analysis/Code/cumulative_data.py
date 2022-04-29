b757ea025e728de24c1665d58a8f461edd5879f2
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2022-04-29 17:15:51 -0400

    Update cumulative_data.py

--- Diodes/HGCAL/Hamburg_Analysis/Code/cumulative_data.py
+++ Diodes/HGCAL/Hamburg_Analysis/Code/cumulative_data.py
@@ -349,7 +349,7 @@ def main():
     cwd = os.getcwd() 
 
     # Left Fit Estimation
-    left_fit_bias = [100, 130] 
+    left_fit_bias = [90, 110] 
     right_fit_length = 100
 
     plot = True
@@ -369,9 +369,9 @@ def main():
     # Get Depletion Values
     CV = find_depletion_voltage(CV, diode_names, left_fit_bias, plot, right_fit_length)
 
-    #CV['N4789_24_LR_DIODE_GR_Irradiated_60C_130min']['Dep_V'] = 
-    #CV['N4789_24_LR_DIODEHALF_GR_Irradiated_60C_130min']['Dep_V'] = 
-    #CV['N4789_24_LR_DIODEQUARTER_GR_Irradiated_60C_130min']['Dep_V'] = 
+    #CV['N4789_24_LR_DIODE_GR_Irradiated_60C_130min']['Dep_V'] = 200
+    #CV['N4789_24_LR_DIODEHALF_GR_Irradiated_60C_130min']['Dep_V'] = 300
+    #CV['N4789_24_LR_DIODEQUARTER_GR_Irradiated_60C_130min']['Dep_V'] = 400
 
     # Get Current at Depletion Voltage
     IV = get_current_at_depv(IV, CV, diode_names)
